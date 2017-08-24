dbefdd88dc000c280d9ebe57d23e8ec047b65538
taquino <aquinotamara90@gmail.com>
2017-08-24 02:41:39 -0300

    clase 15/8

---
+++ src/MainApp.java
@@ -0,0 +1,9 @@
+import Vista.VentanaCalculadora;
+
+public class MainApp {
+	public static void main(String[] args) {
+		VentanaCalculadora calculadora = new VentanaCalculadora();
+		calculadora.setVisible(true);
+	}
+
+}
